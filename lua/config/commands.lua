50c9fd1c354fa19f78f4140803c0cb0b60d3b1ed
Maximilian Barth <maximilian@barth.zip>
2025-03-14 14:41:27 +0100

    add command

--- lua/config/commands.lua
+++ lua/config/commands.lua
@@ -5,3 +5,7 @@ end, {})
 vim.api.nvim_create_user_command("Light", function()
   vim.opt.background = "light"
 end, {})
+
+vim.api.nvim_create_user_command("Pin", function()
+  vim.lsp.buf.execute_command({ command = 'tinymist.pinMain', arguments = { vim.api.nvim_buf_get_name(0) } })
+end, {})
